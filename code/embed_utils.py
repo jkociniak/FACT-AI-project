211686d4dec0de700548a5d994c72409494403fd
Job <jjhlgraber@gmail.com>
2023-01-16 11:57:13 +0100

    added saving and loading embeddings

--- code/embed_utils.py
+++ code/embed_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import networkx as nx
 from karateclub import DeepWalk
 
@@ -20,3 +21,28 @@ def graph2embed(graph, method="karateclub"):
         model.fit(graph)
         embed = model.get_embedding()
         return embed
+
+
+def save_embed(embed: np.array, dataset: str, method: str, implementation: str):
+    """"
+    Saves an embedding to the appropriate directionary and file. 
+    TODO Maybe hyperparameter info like the number of embedding dimensions should be added
+    For now without pickle for possible compatability issues. 
+    """
+    path = f"embeddings/{dataset}/{dataset}_{method}_{implementation}"
+    np.save(path, embed, allow_pickle=False)
+
+
+def load_embed(dataset: str, method: str, implementation: str):
+    """"
+    load an embedding. 
+    """
+    path = f"embeddings/{dataset}/{dataset}_{method}_{implementation}"
+    if implementation=="perozzi":
+        embed = np.genfromtxt(path + ".embeddings", dtype=np.single, skip_header=1, usecols=np.arange(1,65))
+        indices = np.genfromtxt(path + ".embeddings", dtype=np.uintc, skip_header=1, usecols=[0])
+        # sort the embeddings
+        embed = embed[indices]
+    else:
+        embed = np.load(path + ".npy", allow_pickle=False)
+    return embed
