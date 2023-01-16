19ed9f46c66132fd869135e8559815fb430bfc2b
Job <jjhlgraber@gmail.com>
2023-01-16 14:03:25 +0100

    added check_input_formatting()

--- code/embed_utils.py
+++ code/embed_utils.py
@@ -4,8 +4,22 @@ from os.path import splitext
 import networkx as nx
 from karateclub import DeepWalk
 
+def check_input_formatting(**kwargs):
+    """
+    Check that the input strings follow the correct naming conventions
+    """
+    if "dataset" in kwargs:
+        assert kwargs['dataset'] in ["rice", "synth_3layers", "synth2", "synth3", "twitter"], "\
+            dataset should be either 'rice', 'synth_3layers', 'synth2', 'synth3'or 'twitter'"
+    if "method" in kwargs:
+        assert kwargs["method"] in ["deepwalk"], "method should be 'deepwalk' (for now)"
+    if "implementation" in kwargs:
+        assert kwargs["implementation"]in ["karateclub"], "implementation should be 'karateclub' \
+            (for now)"
+
 
 def data2graph(dataset: str, attribute_name: str):
+    check_input_formatting(dataset=dataset)
     path = f"../data/{dataset}/"
     path += splitext(listdir(path)[0])[0]
     # TODO? Datasets may have more than one attribute (or is that case not relevant here?)
@@ -20,14 +34,14 @@ def data2graph(dataset: str, attribute_name: str):
     return G
 
 
-def graph2embed(graph, method="karateclub"):
-    if method=="karateclub":
-        model = DeepWalk()
-        model.fit(graph)
-        embed = model.get_embedding()
-        return embed
-    else:
-        raise Exception("Unknown method. Try 'karateclub'.")
+def graph2embed(graph, method="deepwalk", implementation="karateclub"):
+    check_input_formatting(method=method, implementation=implementation)
+    if method == "deepwalk":
+        if implementation == "karateclub":
+            model = DeepWalk()
+            model.fit(graph)
+            embed = model.get_embedding()
+    return embed
 
 
 def save_embed(embed: np.array, dataset: str, method: str, implementation: str):
@@ -36,6 +50,7 @@ def save_embed(embed: np.array, dataset: str, method: str, implementation: str):
     TODO Maybe hyperparameter info like the number of embedding dimensions should be added
     For now without pickle for possible compatability issues. 
     """
+    check_input_formatting(dataset=dataset, method=method, implementation=implementation)
     path = f"embeddings/{dataset}/{dataset}_{method}_{implementation}"
     np.save(path, embed, allow_pickle=False)
 
@@ -44,6 +59,7 @@ def load_embed(dataset: str, method: str, implementation: str):
     """"
     load an embedding. 
     """
+    check_input_formatting(dataset=dataset, method=method, implementation=implementation)
     path = f"embeddings/{dataset}/{dataset}_{method}_{implementation}"
     if implementation=="perozzi":
         # this doesn't work yet, since the perozzi implementation seems to discard unconnected nodes
@@ -54,5 +70,5 @@ def load_embed(dataset: str, method: str, implementation: str):
         embed = embed[indices]
     else:
         embed = np.load(path + ".npy", allow_pickle=False)
-
+        
     return embed
