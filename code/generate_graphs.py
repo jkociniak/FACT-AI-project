89402269ea677ee12f8e71c99e96ab974cca25df
Job <jjhlgraber@gmail.com>
2023-01-30 18:33:18 +0100

    Added visualization

--- code/generate_graphs.py
+++ code/generate_graphs.py
@@ -120,3 +120,37 @@ def density(G):
 
 #     print(i, r, d, diff, len(G.edges()))
 #     i = i + 1
+
+
+# # Visualization
+# # Taken from https://networkx.org/documentation/stable/auto_examples/drawing/plot_degree.html
+# degree_sequence = sorted((d for n, d in G.degree()), reverse=True)
+# dmax = max(degree_sequence)
+
+# fig = plt.figure("Degree of a random graph", figsize=(8, 8))
+# # Create a gridspec for adding subplots of different sizes
+# axgrid = fig.add_gridspec(5, 4)
+
+# ax0 = fig.add_subplot(axgrid[0:3, :])
+# # Gcc = G.subgraph(sorted(nx.connected_components(G), key=len, reverse=True)[0])
+# Gcc = G
+# pos = nx.spring_layout(Gcc, seed=10396953)
+# nx.draw_networkx_nodes(Gcc, pos, ax=ax0, node_size=20)
+# nx.draw_networkx_edges(Gcc, pos, ax=ax0, alpha=0.4)
+# ax0.set_title("Connected components of G")
+# ax0.set_axis_off()
+
+# ax1 = fig.add_subplot(axgrid[3:, :2])
+# ax1.plot(degree_sequence, "b-", marker="o")
+# ax1.set_title("Degree Rank Plot")
+# ax1.set_ylabel("Degree")
+# ax1.set_xlabel("Rank")
+
+# ax2 = fig.add_subplot(axgrid[3:, 2:])
+# ax2.bar(*np.unique(degree_sequence, return_counts=True))
+# ax2.set_title("Degree histogram")
+# ax2.set_xlabel("Degree")
+# ax2.set_ylabel("# of Nodes")
+
+# fig.tight_layout()
+# plt.show()
