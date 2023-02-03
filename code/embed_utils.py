82a21e385c5c4cc00934990dc904b8ae4dd7d8a8
janko <jan.kociniak@gmail.com>
2023-02-03 01:09:57 +0100

    fixed crosswalk in embed utils, final script for IM experiments reproduction

--- code/embed_utils.py
+++ code/embed_utils.py
@@ -117,118 +117,19 @@ def data2graph(dataset: str):
     return graph
 
 
-def reweight_edges(graph, reweight_method, alpha=0.5, p=4):
-    """
-    reweight edge weights using either fairwalk or crosswalk
-    Note that this does not normalize the weights, as that is done later in graph2embed
-    by preprocess_transition_probs() anyway
-    TODO allow alpha and p to be input
-    """
-    # Initiate new directed graph to store the new weights
-    d_graph = graph.to_directed()
-    node2group = nx.get_node_attributes(d_graph, SENSATTR)
-
-    if reweight_method == "fairwalk":
-        for node in d_graph.nodes:
-            # Collect density for the groups of the neighbors
-            density_per_group = defaultdict(lambda: 0)
-            for neighbor in d_graph.neighbors(node):
-                density_per_group[node2group[neighbor]] += graph[node][neighbor][
-                    "weight"
-                ]
-
-            # Compute the new weights
-            for neighbor in d_graph.neighbors(node):
-                old_weight = graph[node][neighbor]["weight"]
-                new_weight = old_weight / density_per_group[node2group[neighbor]]
-                d_graph[node][neighbor]["weight"] = new_weight
-
-    elif reweight_method == "crosswalk":
-        # This follows equation 3 and from the paper
-        # Estimate a measure of proximity (m) for each node to other groups in the graph
-        # Generate the walks to estimate m for all nodes at once
-        walks = walker.random_walks(
-            d_graph, n_walks=R, walk_len=D, start_nodes=d_graph.nodes, verbose=False
-        )
-
-        # Precompute factors that are in the list comprehension
-        n_nodes = d_graph.number_of_nodes()
-        total_walks = walks.shape[0]
-        denominator = R * D
-
-        # Compute proximity for each batch of walks with the same starting node
-        proximities = []
-        for node in d_graph.nodes:
-            proximity = 0
-            for walk in walks[np.arange(node, total_walks, n_nodes)]:
-                prox += walk.tolist().count(node2class[node])
-            proximities.append(prox / denominator + 0.00001)
-
-        # Compute the new weights
-        for node in d_graph.nodes():
-            # Split the neighbors based on whether they share the class of the source node
-            neighbors_classes = Counter()
-            for neighbor in d_graph.neighbors(node):
-                neighbors_classes[node2class[neighbor]] += 1
-
-            if not neighbors_classes:
-                print('Node {} has no neighbors.'.format(node))
-                continue
-
-            if node2class[node] in neighbors_classes and len(neighbors_classes) == 1:
-                print('Node {} has no neighbors of a different class.'.format(node))
-                denominator = sum(
-                    graph[node][neighbor]["weight"] * proximities[neighbor] ** p
-                    for neighbor in d_graph.neighbors(node)
-                )
-                for neighbor in d_graph.neighbors(node):
-                    old_weight = graph[node][neighbor]["weight"]
-                    new_weight = (
-                        old_weight * proximities[neighbor] ** p  # we drop multiplication by (1-alpha) here bc we have only 1 group
-                        / denominator
-                    )
-                    d_graph[node][neighbor]["weight"] = new_weight
-                continue
-
-            for cls in neighbors_classes:
-                cls_neighbors = [u for u in d_graph.neighbors(node) if node2class[u] == cls]
-                denominator = sum(
-                    graph[node][neighbor]["weight"] * proximities[neighbor] ** p
-                    for neighbor in cls_neighbors
-                )
-
-                # we excluded the situation where node has only neighbors of the same class
-                # so we have three more cases to consider
-                if cls == node2class[node]:  # neighbor of the same class, we multiply by (1-alpha) bc we are sure that we have at least one other group
-                    coeff = (1 - alpha)
-                else:
-                    if node2class[node] in neighbors_classes:  # neighbor of a different class
-                        coeff = alpha / (len(neighbors_classes) - 1)
-                    else:  # neighbor of a different class, but node has only neighbors of a different classes, so we don't need to multiply by alpha and subtract 1 from number of classes
-                        coeff = 1 / len(neighbors_classes)
-
-                for neighbor in cls_neighbors:
-                    old_weight = graph[node][neighbor]["weight"]
-                    new_weight = (
-                        old_weight
-                        * coeff
-                        * proximities[neighbor] ** p
-                        / denominator
-                    )
-                    d_graph[node][neighbor]["weight"] = new_weight
-
-    return d_graph
-
-
-def graph2embed(graph, reweight_method: str, embed_method: str):
+def graph2embed(graph, reweight_method: str, embed_method: str, reweight_params=None):
     check_input_formatting(
         reweight_method=reweight_method,
         embed_method=embed_method,
     )
-    if reweight_method != "default" and reweight_method != "crosswalk":
-        graph = reweight_edges(graph, reweight_method)
-    kwargs_word2vec = SHARED_WORD2VEC_HYPER.copy()
 
+    if reweight_params is None:
+        reweight_params = {}
+
+    if reweight_method != "default":
+        graph = reweight_edges(graph, reweight_method, **reweight_params)
+
+    kwargs_word2vec = SHARED_WORD2VEC_HYPER.copy()
     if embed_method == "deepwalk":
         kwargs_word2vec.update(DEEPWALK_WORD2VEC_HYPER)
         p = 1
@@ -237,15 +138,17 @@ def graph2embed(graph, reweight_method: str, embed_method: str):
         kwargs_word2vec.update(NODE2VEC_WORD2VEC_HYPER)
         p = P_NODE2VEC
         q = Q_NODE2VEC
-
-    if reweight_method == "crosswalk":
-        walks = crosswalk_randomwalker_BFS(graph, **WALKS_HYPER)
     else:
-        # Generate random walks
-        walks = walker.random_walks(graph, p=p, q=q, verbose=False, **WALKS_HYPER)
+        # should never happen due to asserts in check_input_formatting TODO remove?
+        raise ValueError("embed_method should be 'deepwalk' or 'node2vec'")
+
+    # Generate random walks
+    walks = walker.random_walks(graph, p=p, q=q, verbose=False, **WALKS_HYPER)
     walks = [list(map(str, walk)) for walk in walks]
+
     # Generate embeddings
     model = Word2Vec(walks, **kwargs_word2vec)
+
     # Ensure that the indices stay consistent
     original_graph_indices = [
         model.wv.key_to_index[str(i)] for i in range(model.wv.__len__())
@@ -254,6 +157,194 @@ def graph2embed(graph, reweight_method: str, embed_method: str):
     embed = model.wv[original_graph_indices].copy()
 
     return embed
+
+
+def reweight_edges(graph, reweight_method, **kwargs):
+    """
+    reweight edge weights using either fairwalk or crosswalk
+    Note that this does not normalize the weights, as that is done later in graph2embed
+    by preprocess_transition_probs() anyway
+    TODO allow alpha and p to be input
+    """
+
+    if reweight_method == "fairwalk":
+        d_graph = reweight_fairwalk(graph)
+    elif reweight_method == "crosswalk":
+        d_graph = reweight_crosswalk(graph, **kwargs)
+    # elif reweight_method == 'crosswalk_original_bfs':
+    #     d_graph = reweight_crosswalk_original_bfs(graph)
+    else:
+        raise ValueError("reweight_method should be 'fairwalk' or 'crosswalk'")
+
+    return d_graph
+
+
+def reweight_fairwalk(graph):
+    # Initiate new directed graph to store the new weights
+    d_graph = graph.to_directed()
+    node2group = nx.get_node_attributes(d_graph, SENSATTR)
+
+    for node in d_graph.nodes:
+        # Collect density for the groups of the neighbors
+        density_per_group = defaultdict(lambda: 0)
+        for neighbor in d_graph.neighbors(node):
+            density_per_group[node2group[neighbor]] += graph[node][neighbor][
+                "weight"
+            ]
+
+        # Compute the new weights
+        for neighbor in d_graph.neighbors(node):
+            old_weight = graph[node][neighbor]["weight"]
+            new_weight = old_weight / density_per_group[node2group[neighbor]]
+            d_graph[node][neighbor]["weight"] = new_weight
+
+    return d_graph
+
+
+def reweight_crosswalk(graph, alpha=0.5, p=4):
+    d_graph = graph.to_directed()
+    node2group = nx.get_node_attributes(d_graph, SENSATTR)
+
+    # This follows equation 3 and from the paper
+    # Estimate a measure of proximity (m) for each node to other groups in the graph
+    # Generate the walks to estimate m for all nodes at once
+    walks = walker.random_walks(
+        d_graph, n_walks=R, walk_len=D, start_nodes=d_graph.nodes, verbose=False
+    )
+
+    # Precompute factors that are in the list comprehension
+    n_nodes = d_graph.number_of_nodes()
+    total_walks = walks.shape[0]
+    denominator = R * D
+
+    # Compute proximity for each batch of walks with the same starting node
+    proximities = []
+    for node in d_graph.nodes:
+        prox = 0
+        for walk in walks[np.arange(node, total_walks, n_nodes)]:
+            prox += walk.tolist().count(node2group[node])
+        proximities.append(prox / denominator + 0.00001)
+
+    # Compute the new weights
+    for node in d_graph.nodes():
+        # Split the neighbors based on whether they share the class of the source node
+        neighbors_groups = Counter()
+        for neighbor in d_graph.neighbors(node):
+            neighbors_groups[node2group[neighbor]] += 1
+
+        if not neighbors_groups:
+            print(f'Node {node} has no neighbors.')
+            continue
+
+        if node2group[node] in neighbors_groups and len(neighbors_groups) == 1:
+            denominator = sum(
+                graph[node][neighbor]["weight"] * proximities[neighbor] ** p
+                for neighbor in d_graph.neighbors(node)
+            )
+            for neighbor in d_graph.neighbors(node):
+                old_weight = graph[node][neighbor]["weight"]
+                new_weight = (
+                        old_weight * proximities[
+                    neighbor] ** p  # we drop multiplication by (1-alpha) here bc we have only 1 group
+                        / denominator
+                )
+                d_graph[node][neighbor]["weight"] = new_weight
+            continue
+
+        print(f'There is at least one neighbor of {node} in a different group than {node}.')
+        for group in neighbors_groups:
+            print(f'Processing neighbors of group {group}')
+            neighbors_in_group = [u for u in d_graph.neighbors(node) if node2group[u] == group]
+            denominator = sum(
+                graph[node][neighbor]["weight"] * proximities[neighbor] ** p
+                for neighbor in neighbors_in_group
+            )
+
+            # we excluded the situation where node has only neighbors of the same class
+            # so we have three more cases to consider
+            if group == node2group[
+                node]:  # neighbor of the same class, we multiply by (1-alpha) bc we are sure that we have at least one other group
+                coeff = (1 - alpha)
+            else:
+                if node2group[node] in neighbors_groups:  # neighbor of a different class
+                    coeff = alpha / (len(neighbors_groups) - 1)
+                else:  # neighbor of a different class, but node has only neighbors of a different classes, so we don't need to multiply by alpha and subtract 1 from number of classes
+                    coeff = 1 / len(neighbors_groups)
+
+            for neighbor in neighbors_in_group:
+                old_weight = graph[node][neighbor]["weight"]
+                new_weight = (
+                        old_weight
+                        * coeff
+                        * proximities[neighbor] ** p
+                        / denominator
+                )
+                d_graph[node][neighbor]["weight"] = new_weight
+
+    return d_graph
+
+# def crosswalk_original_bfs(graph, n_walks=10, walk_len=80):
+#     """
+#     based on crosswalk in the original code
+#     """
+#     node2group = nx.get_node_attributes(graph, SENSATTR)
+#     border_distance = defaultdict(lambda: np.inf)
+#     proximity = defaultdict(lambda: 0)
+#     for group in set(node2group.values()):
+#         # we run bfs starting from each node of the group and collect distances from the border
+#         queue = [v for v in graph if node2group[v] == group]
+#         head = 0
+#         dis = {v: 0 for v in queue}
+#         while head < len(queue):
+#             cur = queue[head]
+#             d_cur = dis[cur]
+#             for u in graph[cur]:
+#                 if (node2group[u] != group) and (u not in dis):
+#                     queue.append(u)
+#                     dis[u] = d_cur + 1
+#                     border_distance[u] = d_cur + 1
+#             head += 1
+#
+#     rand = random.Random()
+#     # I assume this is alpha
+#     p_ch = 0.5
+#     paths = []
+#     for _ in range(n_walks):
+#         for start in graph.nodes:
+#             path = [start]
+#             while len(path) < walk_len:
+#                 cur = path[-1]
+#                 if len(graph[cur]) > 0:
+#                     if border_distance[cur] == 1:
+#                         l_1 = [
+#                             u for u in graph[cur] if node2group[u] == node2group[cur]
+#                         ]
+#                         l_2 = [
+#                             u for u in graph[cur] if node2group[u] != node2group[cur]
+#                         ]
+#                     else:
+#                         l_1 = [
+#                             u
+#                             for u in graph[cur]
+#                             if border_distance[u] >= border_distance[cur]
+#                         ]
+#                         l_2 = [
+#                             u
+#                             for u in graph[cur]
+#                             if border_distance[u] < border_distance[cur]
+#                         ]
+#                     if (len(l_1) == 0) or (len(l_2) == 0):
+#                         choice = rand.choice(list(graph[cur].keys()))
+#                         path.append(choice)
+#                     else:
+#                         if np.random.rand() < p_ch:
+#                             path.append(rand.choice(l_2))
+#                         else:
+#                             path.append(rand.choice(l_1))
+#                 else:
+#                     break
+#             paths.append(path)
+#     return paths
 
 
 def get_embedding_path(dataset: str, reweight_method: str, embed_method: str):
--- code/influence_maximization.py
+++ code/influence_maximization.py
@@ -4,26 +4,54 @@ import numpy as np
 from collections import Counter
 from independent_cascade.independent_cascade import run_ic, get_kmedoids_seeds
 from embed_utils import data2graph, SENSATTR, graph2embed
+import json
 
 
-def calculate_metrics(inf_num, inf_num_grouped, group_counts):
-    # logging.debug(f'Total mean influence (nodes): {inf_num}')
-    print(f'Total mean influence (%): {100 * inf_num / len(graph.nodes)}%')
+def run_experiment(dataset,
+                   ic_activation_prob,
+                   ic_activation_prob_attr,
+                   seed_method,
+                   seed_params,
+                   infmax_params):
+    graph = data2graph(dataset)
+    nx.set_edge_attributes(graph, ic_activation_prob, ic_activation_prob_attr)
+    print(f'constructed {dataset} dataset')
 
-    # logging.debug(f'Mean influence in each group (nodes): {inf_num_grouped}')
-    inf_num_grouped_frac = {g: 100 * inf_num_group / group_counts[g] for g, inf_num_group in inf_num_grouped.items()}
-    # logging.debug(f'Mean influence in each group (%): {inf_num_grouped_frac}')
+    if seed_method == 'load_greedy':
+        seeds = load_greedy_seeds(dataset, **seed_params)
+    elif seed_method == 'kmedoids':
+        graph_emb = graph2embed(graph, **seed_params)
+        seeds = get_kmedoids_seeds(graph_emb, 40)
+    else:
+        raise ValueError(f'Unknown seed method: {seed_method}')
 
-    disparity = np.var(list(inf_num_grouped_frac.values()))
+    print(f'Loaded seeds: {seeds}')
+
+    inf, disp = run_infmax(graph, seeds, ic_activation_prob_attr, **infmax_params)
+    print(f'Influence scores: {inf}, disparities: {disp}')
+    print(f'Mean influence: {np.mean(inf)}, mean disparity: {np.mean(disp)} over {len(inf)} experiments')
+    print()
+
+    return inf, disp
 
 
+def load_greedy_seeds(dataset, seeds_dir):
+    seeds_name = f'{dataset}_greedy_seeds.txt'
+    seeds_path = os.path.join(seeds_dir, seeds_name)
+    print(f'Loading seeds from {seeds_path}')
+    with open(seeds_path, 'r') as f:
+        seeds = eval(f.read())  # the seeds are saved as a list of integers
+    return seeds
 
-def run_infmax(graph, seeds, n_experiments=5, ic_sample_size=500):
+
+def run_infmax(graph, seeds, activation_prob_attr, n_experiments=5, ic_sample_size=500):
     all_counter = Counter()
     for v, sens_val in graph.nodes(data=SENSATTR):
         all_counter[sens_val] += 1
 
     # we run the experiments
+    influences = []
+    disparities = []
     for i in range(n_experiments):
         print(f'Experiment {i + 1}/{n_experiments}')
 
@@ -39,36 +67,90 @@ def run_infmax(graph, seeds, n_experiments=5, ic_sample_size=500):
             mean_inf_nums[g] = count / ic_sample_size
 
         inf_frac = 100 * sum(mean_inf_nums.values()) / len(graph)
+        influences.append(inf_frac)
         print(f'Total mean influence (%): {inf_frac}%')
 
         inf_frac_grouped = {g: 100 * mean_inf_nums[g] / all_counter[g] for g in mean_inf_nums}
         disparity = np.var(list(inf_frac_grouped.values()))
+        disparities.append(disparity)
         print(f'Disparity: {disparity}')
         print()
+    return influences, disparities
 
 
 if __name__ == '__main__':
-    real_datasets = ['rice', 'twitter', 'stanford']
-    synth_datasets = ['synth_3layers', 'synth2', 'synth3']
-    datasets = synth_datasets + real_datasets
-
+    # common settings
+    infmax_params = {'n_experiments': 5, 'ic_sample_size': 500}
     activation_prob_attr = 'ic_activation_prob'
-    seeds_dir = '../results/influence_maximization/greedy_seeds'
+    save_dir = '../results/influence_maximization/bigbatch'
+    embed_method = 'deepwalk'
 
-    for dataset in datasets[3:4]:
-        graph = data2graph(dataset)
-        ic_activation_prob = 0.01 if dataset in real_datasets else 0.03
-        nx.set_edge_attributes(graph, ic_activation_prob, activation_prob_attr)
-        print(f'constructed {dataset} dataset')
+    # list of (dataset, activation_prob) tuples
+    real_datasets = ['rice',
+                     'twitter']
+                     #'stanford']
+    synth_datasets = [#'synth_3layers',
+                      'synth2',
+                      'synth3']
 
-        # seeds_name = f'{dataset}_greedy_seeds.txt'
-        # seeds_path = os.path.join(seeds_dir, seeds_name)
-        # print(f'Loading seeds from {seeds_path}')
-        # with open(seeds_path, 'r') as f:
-        #     seeds = eval(f.read())  # the seeds are saved as a list of integers
+    # possible reweighing strategies
+    reweight_methods = ['default', 'fairwalk', 'crosswalk']
 
-        graph_emb = graph2embed(graph, reweight_method='fairwalk', embed_method='deepwalk')
-        seeds = get_kmedoids_seeds(graph_emb, 40)
-        print(f'Loaded seeds: {seeds}')
+    repetitions = 100
 
-        run_infmax(graph, seeds, n_experiments=5)
+    for dataset in synth_datasets + real_datasets:
+        ap = 0.01 if dataset in real_datasets else 0.03
+
+        # greedy seeds
+        # seed_params = {'seeds_dir': '../results/influence_maximization/greedy_seeds'}
+        # run_experiment(dataset, ap, activation_prob_attr, 'load_greedy', seed_params, infmax_params, save_path)
+
+        # save_name = f'{dataset}_greedy.json'
+        # save_path = os.path.join(save_dir, save_name)
+        # if save_path is not None:
+        #     print(f'Saving results to {save_path}')
+        #     with open(save_path, 'w') as f:
+        #         json.dump(results, f, indent=4)
+
+        # kmedoids seeds
+        for rm in reweight_methods:
+            seed_params = {'reweight_method': rm, 'embed_method': embed_method}
+            if rm == 'crosswalk':
+                seed_params['reweight_params'] = {
+                    'alpha': 0.5 if dataset in real_datasets else 0.7,
+                    'p': 2 if dataset == 'twitter' else 4
+                }
+
+            results = {
+                'settings': {
+                    'dataset': dataset,
+                    'ic_activation_prob': ap,
+                    'ic_activation_prob_attr': activation_prob_attr,
+                    'seed_method': 'kmedoids',
+                    'seed_params': seed_params,
+                    'infmax_params': infmax_params
+                },
+            }
+
+            infs, disps = [], []
+            for i in range(repetitions):  # averaging over 1000 different seed sets
+                inf, disp = run_experiment(dataset, ap, activation_prob_attr, 'kmedoids', seed_params, infmax_params)
+                infs.append(np.mean(inf))
+                disps.append(np.mean(disp))
+
+            results['influences'] = infs
+            results['inf_mean'] = np.mean(infs)
+            results['inf_var'] = np.var(infs)
+            results['disparities'] = disps
+            results['disp_mean'] = np.mean(disps)
+            results['disp_var'] = np.var(disps)
+
+            save_name = f'{dataset}_{rm}_mean_over_{repetitions}_seedsets.json'
+            save_path = os.path.join(save_dir, save_name)
+            if save_path is not None:
+                print(f'Saving results to {save_path}')
+                with open(save_path, 'w') as f:
+                    json.dump(results, f, indent=4)
+
+
+
